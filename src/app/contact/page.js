ecab9182ed4063feb9850c5f7785520311c03361
aimahusnain <aimahusnain@gmail.com>
2023-12-01 13:58:57 +0500

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,28 @@
+import dynamic from 'next/dynamic';
+import ContactForm from "@/src/components/Contact/ContactForm";
+import siteMetadata from "@/src/utils/siteMetaData";
+
+export const metadata = {
+  title: "Contact Me",
+  description: `Contact me through the form available on this page or email me at email`,
+};
+
+const DynamicLottieAnimation = dynamic(() => import('@/src/components/Contact/LottieAnimation'), {
+  ssr: false, // Set ssr to false to prevent server-side rendering
+});
+
+export default function Contact() {
+  return (
+    <>
+      <section className="w-full h-auto md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light">
+        <div className="inline-block w-full sm:w-4/5 md:w-2/5 h-full md:border-r-2 border-solid border-dark dark:border-light">
+          <DynamicLottieAnimation />
+        </div>
+        <div className="w-full md:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 pb-8">
+          <h2 className="font-bold capitalize text-2xl xs:text-3xl sm:text-4xl">Let's Connect!</h2>
+          <ContactForm />
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+  author: 'CodeBucks',
+  headerTitle: 'Next.js Blog',
+  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'http://localhost:3000', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'aimahusnain@gmail.com',
+  github: 'https://github.com/aimahusnain',
+  twitter: 'https://twitter.com/aimahusnain',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com/aimahusnain',
+  linkedin: 'https://www.linkedin.com/in/aimahusnain/',
+  dribbble: 'https://www.dribbble.com/aimahusnain',
+  locale: 'en-US',
+}
+
+module.exports = siteMetadata
